f987a1a837102b5b7f3750a6ef10668a19798648
khoilvgcc210040 <Khoilvgcc210040@fpt.edu.vn>
2023-06-15 08:40:07 +0700

    change database

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=atn_company';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'mysql:host=i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=z8dlnrbiv77deinc';
+$user_name = 'qm43o4mro4m1ozzf';
+$user_password = 'oc34c4keqw90qoe4';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
